cda2d3a85b26c3f93bc0e47d1a1f419e65afac0a
carlio <git@carlcrowder.com>
2017-03-26 02:06:41 +0800

    Very miild py3/2 compatability for the debug statement while this library matures

--- drdocs/entry.py
+++ drdocs/entry.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 import sys
 from drdocs.parse import parse_file
 
@@ -7,4 +8,4 @@ def run():
     path = sys.argv[1]
     things = parse_file(path)
     for t in things:
-        print t.get_messages()
+        print (t.get_messages())
